fcdd909a4bfb9e13b4268583c9b0cdf856adde05
moses-gbondo <37417102+moses-gbondo@users.noreply.github.com>
2018-03-18 01:06:09 -0400

    Update Tree_Traversals.py

--- Data Structures/Binary Tree/Traversals/Tree_Traversals.py
+++ Data Structures/Binary Tree/Traversals/Tree_Traversals.py
@@ -1,1 +1,43 @@
-def 
+class Node:
+    def __init__(self, key=None):
+        self.val = key
+        self.left = None
+        self.right = None
+        self.inorder_array = []
+        self.preorder_array = []
+        self.postorder_array = []
+
+    def inorder(self, root):
+        if root:
+            self.inorder(root.left)
+            self.inorder_array.append(root.val)
+            self.inorder(root.right)
+
+    def preorder(self, root):
+        if root:
+            self.preorder_array.append(root.val)
+            self.preorder(root.left)
+            self.preorder(root.right)
+
+
+    def postorder(self, root):
+        if root:
+            self.postorder(root.left)
+            self.postorder(root.right)
+            self.postorder_array.append(root.val)
+
+
+def main():
+    root = Node(1)
+    root.left = Node(2)
+    root.right = Node(3)
+    root.left.left = Node(4)
+    root.left.right = Node(5)
+    obj = Node()
+    obj.inorder(root)
+    obj.preorder(root)
+    obj.postorder(root)
+    print("Inorder ", obj.inorder_array)
+    print("Postorder ", obj.postorder_array)
+    print("Preorder ", obj.preorder_array)
+main()
