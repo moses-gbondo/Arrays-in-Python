b405785e1b129f912c265cedb5745a89f7e84a2b
moses-gbondo <37417102+moses-gbondo@users.noreply.github.com>
2018-03-22 13:27:55 -0400

    Create find-the-largest-pair-sum-in-an-unsorted-array.py

---
+++ Data Structures/Array/Order Statistics/find-the-largest-pair-sum-in-an-unsorted-array.py
@@ -0,0 +1,5 @@
+Find the largest pair sum in an unsorted array
+Given an unsorted of distinct integers, find the largest pair sum in it. For example, the largest pair sum in {12, 34, 10, 6, 40} is 74.
+
+Difficulty Level: Rookie
+Expected Time Complexity: O(n) [Only one traversal of array is allowed]
