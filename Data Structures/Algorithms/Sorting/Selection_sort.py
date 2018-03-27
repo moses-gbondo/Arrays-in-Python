db0364d4e8211e3c2470d6d150d699be71235dd5
moses-gbondo <37417102+moses-gbondo@users.noreply.github.com>
2018-03-27 16:48:18 -0400

    Update Selection_sort.py

--- Data Structures/Algorithms/Sorting/Selection_sort.py
+++ Data Structures/Algorithms/Sorting/Selection_sort.py
@@ -1,2 +1,13 @@
 #Selection sort is O(n^2) and selects the minimum element index and swaps it.
 def selection_sort(arr):
+  for i in range(len(arr)):
+    min_indx = i
+    for j in range(i+1, len(arr)):
+      if arr[j] < arr[min_indx]:
+        min_indx = j
+    arr[i], arr[min_indx], = arr[min_indx], arr[i]
+    
+def main():
+    arr = [64, 25, 12, 22 ,11, -1, 4, 9]
+    selection_sort(arr)
+main()
