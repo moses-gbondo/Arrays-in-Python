e1856584e333e7aae9f50484d758ac782d670189
moses-gbondo <37417102+moses-gbondo@users.noreply.github.com>
2018-04-16 23:36:55 -0400

    Update Bubble Sort.py

--- Data Structures/Algorithms/Sorting/Bubble Sort.py
+++ Data Structures/Algorithms/Sorting/Bubble Sort.py
@@ -1,1 +1,7 @@
-def bubble_sort():
+def bubble_sort2():
+  arr = [5,4,1,6,3,2,7]
+  for i in range(len(arr)):
+      for j in range(0, len(arr)-i-1):
+          if arr[j] > arr[j + 1]:
+              arr[j], arr[j+1] = arr[j+1], arr[j]
+  print("This is bubble sort ", arr)
